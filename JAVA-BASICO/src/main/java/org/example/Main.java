81ab6a5fd48e9bec526353abbe9ac0efc1f4f8f8
Nilton Santana d3vB4 <nilton.s.santana@ba.estudante.senai.br>
2026-08-11 21:23:35 -0300

    Add files via upload

---
+++ JAVA-BASICO/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+      // Declaração de variáveis.
+        String nome = "Marta";
+        int idade = 30;
+        double peso = 65.2;
+
+        final String CPF = "123.456.789-00";
+
+        System.out.println("O nome do usuário é:" + nome);
+        System.out.println("O CPF do usuário é:" + CPF);
+        System.out.println("O idade do usuário é:" + idade + " anos ");
+        System.out.println("O peso do usuário é:" + peso + " quilos ");
+
+    }
+}
